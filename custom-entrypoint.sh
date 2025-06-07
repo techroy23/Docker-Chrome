6c86497b54e0dff2b478d8673253066b6f409f5f
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-06-08 07:43:32 +0800

    Update

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -8,7 +8,7 @@ export DISPLAY=$(pgrep -a Xvfb | grep -o ':[0-9]\+')
 URL=${URL:-""}
 
 launch_browser() {
-    /usr/bin/google-chrome-stable --no-first-run --no-default-browser-check --no-managed-user-acknowledgment --no-sandbox --no-process-singleton-dialog --disable-dbus --disable-gpu --ignore-gpu-blocklist --disable-gpu-driver-bug-workarounds --use-gl=swiftshader --enable-unsafe-swiftshader "$1" >/dev/null 2>&1 &
+    /usr/bin/google-chrome-stable --kiosk --no-first-run --no-default-browser-check --no-managed-user-acknowledgment --no-sandbox --no-process-singleton-dialog --disable-dbus --disable-gpu --ignore-gpu-blocklist --disable-gpu-driver-bug-workarounds --use-gl=swiftshader --enable-unsafe-swiftshader "$1" >/dev/null 2>&1 &
 }
 
 if [[ -z "$URL" ]]; then
@@ -22,12 +22,18 @@ discord_loop() {
     DISCORD_WEBHOOK_INTERVAL=${DISCORD_WEBHOOK_INTERVAL:-300}
     local SCREENSHOT_PATH="/tmp/screenshot.png"
     local DISCORD_WEBHOOK_URL="$DISCORD_WEBHOOK_URL"
+    local HOSTNAME="$(HOSTNAME)"
+
     if [[ -z "$DISCORD_WEBHOOK_URL" || ! "$DISCORD_WEBHOOK_URL" =~ ^https://discord\.com/api/webhooks/[0-9]+/[A-Za-z0-9_-]+$ ]]; then
         echo "Invalid or missing Discord webhook URL. Exiting..."
+        return
     fi
+
     while true; do
         scrot -o -D "$DISPLAY" "$SCREENSHOT_PATH"
-        curl -s -o /dev/null -X POST -F "file=@$SCREENSHOT_PATH" "$DISCORD_WEBHOOK_URL"
+        curl -s -o /dev/null -X POST "$DISCORD_WEBHOOK_URL" \
+            -F "file=@$SCREENSHOT_PATH" \
+            -F "payload_json={\"embeds\": [{\"title\": \"Docker Hostname: $HOSTNAME\", \"color\": 5814783}]}"
         sleep "$DISCORD_WEBHOOK_INTERVAL"
     done
 }
